6d352bf5f3c7155bbe3f3bb49419c6209244039f
Llostris <deiidrell@gmail.com>
2015-03-14 22:56:50 +0100

    cleanup

--- src/lab01/test_bandwidth.py
+++ src/lab01/test_bandwidth.py
@@ -5,30 +5,28 @@ import sys
 import numpy
 
 TESTS = 100
-ITERATIONS = 10000
+ITERATIONS = 1000
 BANDWIDTH_FACTOR = 8.0 / (1024 * 1024)
-STEP = 1000
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
 def usage_info():
-    print 'Usage: ./test_comm.py [max size] [standard/synchronous] [tests] [step size]'
-    print 'Requirements: Step size < max size'
+    print 'Usage: ./test_comm.py [max size] [standard/synchronous] [iterations] [tests]'
     sys.exit(-1)
 
 def sendMessageStandard(buffer, buffer_size):
-    for i in xrange(ITERATIONS / buffer_size):  # scale the number of iterations s
+    for i in xrange(ITERATIONS):  # scale the number of iterations s
         comm.Send(buffer[:buffer_size], dest = 1)
         comm.Recv(buffer[:buffer_size], source = 1)
 
 def sendMessageSynchronized(buffer, buffer_size):
-    for i in xrange(ITERATIONS / buffer_size):
+    for i in xrange(ITERATIONS):
         comm.Ssend(buffer[:buffer_size], dest = 1)
         comm.Recv(buffer[:buffer_size], source = 1)
 
 def receiveMessages(buffer, buffer_size):
-    for i in xrange(ITERATIONS / buffer_size):
+    for i in xrange(ITERATIONS):
         status = MPI.Status()
         comm.Recv(buffer[:buffer_size], source = 0, status = status)
         if comm_type == 'standard':
@@ -36,68 +34,53 @@ def receiveMessages(buffer, buffer_size):
         else:
             comm.Ssend(buffer[:buffer_size], dest = 0)
 
-def print_results(results):
-    print 'bandwidth python', package_size, comm_type, ITERATIONS, TESTS
-    size = STEP
-    for result in results:
-        print size, result
-        size += STEP
+def print_results(result):
+    print 'python bandwidth', package_size, comm_type, result
 
 # main program
 
 if len(sys.argv) < 3 :
     usage_info()
 
-package_size = int(sys.argv[1]) + 1
+package_size = int(sys.argv[1])
 comm_type = sys.argv[2]
-ITERATIONS = package_size
-if len(sys.argv) > 3 :
-    TESTS = int(sys.argv[3])
-if len(sys.argv) > 4 :
-    STEP = int(sys.argv[4])
-
 buffer = numpy.arange(package_size, dtype='b')
-bandwidth_sum = {}
-for i in xrange(STEP, package_size, STEP):
-    bandwidth_sum[i] = 0
+if len(sys.argv) > 3 :
+    ITERATIONS = int(sys.argv[3])
+if len(sys.argv) > 4 :
+    TESTS = int(sys.argv[4])
+
+bandwidth_sum = 0
 
 comm.Barrier()
 
 for _ in xrange(TESTS):
 
-    for size in xrange(STEP, package_size, STEP):
+    if rank == 0 :
 
-        if rank == 0 :
+        time_start = MPI.Wtime()
 
-            time_start = MPI.Wtime()
+        if comm_type == 'standard' :
+            sendMessageStandard(buffer, package_size)
+        elif comm_type == 'synchronous' :
+            sendMessageSynchronized(buffer, package_size)
 
-            if comm_type == 'standard' :
-                sendMessageStandard(buffer, size)
-            elif comm_type == 'synchronous' :
-                sendMessageSynchronized(buffer, size)
+        time_end = MPI.Wtime()
 
-            time_end = MPI.Wtime()
+        # gather statistics
+        time = time_end - time_start
+        try:
+            bandwidth = ITERATIONS * package_size * 1.0 / time
+            bandwidth *= 2
+            bandwidth_sum += bandwidth * BANDWIDTH_FACTOR
+        except ZeroDivisionError:
+            pass
 
-            # gather statistics
-            time = time_end - time_start
-            try:
-                bandwidth = ITERATIONS * 1.0 / time
-                bandwidth *= 2
-                bandwidth_sum[size] += bandwidth * BANDWIDTH_FACTOR
-            except ZeroDivisionError:
-                pass
+    elif rank == 1 :
 
-        elif rank == 1 :
-
-            receiveMessages(buffer, size)
-
-        else :
-            print "Expected only two nodes"
+        receiveMessages(buffer, package_size)
 
 if rank == 0:
-    bandwidth_to_size = []
-    for size in xrange(STEP, package_size, STEP):
-        bandwidth_to_size.append(bandwidth_sum[size] / TESTS)
-    print_results(bandwidth_to_size)
+    print_results(bandwidth_sum / TESTS)
 
 MPI.Finalize()
